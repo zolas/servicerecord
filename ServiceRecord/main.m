48d9c2d79859af3f7b433efa18250290a70594d8
Gray <gray@Monster.local>
2014-02-10 16:37:22 -0500

    Initial Commit

---
+++ ServiceRecord/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ServiceRecord
+//
+//  Created by Gray on 2/10/2014.
+//  Copyright (c) 2014 Black Magma Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ServiceRecordTests/ServiceRecordTests.m
@@ -0,0 +1,34 @@
+//
+//  ServiceRecordTests.m
+//  ServiceRecordTests
+//
+//  Created by Gray on 2/10/2014.
+//  Copyright (c) 2014 Black Magma Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ServiceRecordTests : XCTestCase
+
+@end
+
+@implementation ServiceRecordTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
